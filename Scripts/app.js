e73c358c82961368ddbe5d30639fcf84ab7e5ca5
-global <drumsandbass911@yahoo.com>
2016-05-31 09:56:53 -0700

    Added better ui for login

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,5 +1,7 @@
 var myApp = angular.module('myApp',['ngRoute']);
 
+
+
 myApp.config(function ($routeProvider) {
     
     $routeProvider
@@ -18,36 +20,68 @@ myApp.config(function ($routeProvider) {
     
 });
 
-
-myApp.controller('mainController',['$scope',function($scope){
+myApp.service('authService',function(){
+   
+   this.authToken = '';
+   this.isAuth = false;
     
-}]);
+});
 
 
-myApp.controller('loginController',['$scope','$http',function($scope,$http){
+myApp.controller('mainController',['$scope','$http','authService',function($scope,$http,authService){
+    
     $scope.model = {
         email : '',
         password : '',
-        authToken: '',
     };
+    
+    $scope.isAuth = false;
     
     $scope.login = function(){
         $http.post('http://localhost:5000/users/login', {email: $scope.model.email,password: $scope.model.password})
         .success(function(result,status,headers){
             if(status === 200){
                 alert("AUth Token: " + headers().auth);
-            }else{
+                authService.authToken = headers().auth;
+                authService.isAuth = true;
+                $scope.isAuth = true;
+            }else if(status === 401){
+                alert("Unauthorized");
+            }
+            else{
                 alert(status + ": " + result);
             }
         })
         .error(function(data,status,headers,config){
-            alert(status + ": " + data);
+            if(status === 401){
+                alert("Unauthorized");
+            }else{
+                alert("Problem signing in. Please try again");
+            }
+            
         });
     }
+    
 }]);
 
 
-myApp.controller('todoController',['$scope',function($scope){
+myApp.controller('loginController',['$scope','$http','authService',function($scope,$http,authService){
+    
+    $scope.model = {
+        email : '',
+        password : '',
+    };
+}]);
+
+
+myApp.controller('todoController',['$scope','$http','authService',function($scope,$http,authService){
+    
+    if(!authService.isAuth){
+        alert("Not Authenticated");
+    }else{
+        
+    }
+    
     
 }]);
 
